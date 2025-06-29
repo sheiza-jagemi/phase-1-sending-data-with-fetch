11977ace5760378a9e830c9a5801fff84f2988fc
sheiza-jagemi <sheiza.jagemi@student.moringaschool.com>
2025-06-29 13:07:10 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(object => {
+      document.body.innerHTML = object.id;
+    })
+    .catch(error => {
+      document.body.innerHTML = error.message;
+    });
+}
